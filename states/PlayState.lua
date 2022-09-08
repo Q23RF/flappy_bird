62f400c207f3a974e5bad513aff6a1d52b3b944a
李星亞 <elsie094081@gmail.com>
2022-09-08 20:58:35 +0800

    more typos

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -33,13 +33,11 @@ function PlayState:update(dt)
     if self.paused then
         if love.keyboard.wasPressed('space') then
             self.paused = false
-            gSounds['pause']:play()
         else
             return
         end
     elseif love.keyboard.wasPressed('space') then
         self.paused = true
-        gSounds['pause']:play()
         return
     end
 
